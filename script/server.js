8bd110bddf92af16a13c295d6895fd17117d2649
EeehGiji <107210061+Fvnnico@users.noreply.github.com>
2024-04-16 09:08:14 +0200

    Fix database

--- script/server.js
+++ script/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const csrf = require('csurf');
 
 const app = express();
