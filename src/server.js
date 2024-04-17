22012946122d5cc4d91dc49ce0d247ee09e3e1fb
EeehGiji <107210061+Fvnnico@users.noreply.github.com>
2024-04-17 12:21:35 +0200

    nodemon login

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const bodyParser = require('body-parser');  // per il middleware
+const mysql = require('mysql2');        // database
 const path = require('path');  // test
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');      // password hash
+
 
 const app = express();
 const port = 3300;
@@ -62,11 +63,22 @@ authRouter.get('/registrazione', (req, res) => {
     });
 });
 
+// Pagina di login routing
+authRouter.get('/login', (req, res) => {
+    const filePath = path.join(__dirname, 'pages', 'login.html');
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Errore interno del server');
+        }
+    });
+});
+
+
 
 // Quando clicchi registrazione bottone
 app.post('/registrazione', (req, res) => {
     
-
     const { nome, cognome, email, password } = req.body;
 
     // Crittografa la password prima di memorizzarla nel database
@@ -96,17 +108,44 @@ app.post('/registrazione', (req, res) => {
 
 
 
+// Gestione del login
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
-// Pagina di login
-authRouter.get('/login', (req, res) => {
-    const filePath = path.join(__dirname, 'pages', 'login.html');
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Errore interno del server');
+    const query = 'SELECT * FROM Utenti WHERE email = ?';
+    connection.query(query, [email], (error, results, fields) => {
+        if (error) {
+            console.error('Errore durante il login:', error);
+            return res.status(500).send('Errore durante il login.');
         }
+
+        // Controlla se l'utente è stato trovato
+        if (results.length === 0) {
+            // L'utente non esiste, reindirizza alla pagina di login con un messaggio di errore
+            return res.send('Utente non trovato.');
+
+        }
+
+        // Utente trovato, verifica la password
+        const user = results[0];
+        bcrypt.compare(password, user.password, (err, passwordMatch) => {
+            if (err) {
+                console.error('Errore durante il confronto delle password:', err);
+                return res.status(500).send('Errore durante il login.');
+            }
+
+            if (passwordMatch) {
+                const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' }); // Genera un token JWT
+                res.cookie('authToken', token, { httpOnly: true }); // Memorizza il token come cookie
+                res.redirect('/'); // Reindirizza alla home page o ad altra pagina di successo
+            } else  if(!passwordMatch){
+                // La password non corrisponde, reindirizza alla pagina di login con un messaggio di errore
+                console.log('non corrisponde password (non trova bo)');
+            }
+        });
     });
 });
+
 
 
 // per errori below
