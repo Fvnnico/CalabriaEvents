595cd6bdd52397177fbd74316c1e4c7d29c1cf38
EeehGiji <107210061+Fvnnico@users.noreply.github.com>
2024-04-20 14:56:12 +0200

    dropdown hamburger

---
+++ src/scripts/dropdownHamburger.js
@@ -0,0 +1,28 @@
+
+const btn = document.getElementById("hamburger");
+const img = document.getElementById("hamburger");
+
+function apriDropdown() {
+    const dropdown = document.getElementById("dropdownApparso");    
+    dropdown.classList.toggle("show");
+    
+    if (dropdown.classList.contains("show")) {
+        img.setAttribute("src", "/pictures/x-chiudi.png");
+    } else {
+        img.setAttribute("src", "/pictures/hamburger.png"); // Icona Hamburger
+    }
+}
+
+
+window.onclick = function (event) {
+    if (!event.target.matches(".hamburger-menu")) {
+        let dropdowns = document.getElementsByClassName("dropdownApparso");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+                img.setAttribute("src", "/pictures/hamburger.png");
+            }
+        }
+    }
+};
