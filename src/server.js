8be7bcaba68eeaf277d1882402d0e9573bce6b04
EeehGiji <107210061+Fvnnico@users.noreply.github.com>
2024-04-24 12:51:09 +0200

    allah
    wallahi

--- src/server.js
+++ src/server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const multer = require("multer"); // per caricare dati
 const bodyParser = require("body-parser"); // per il middleware
 
-
 const app = express();
-const port = 3300;
+const port = 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,25 +20,41 @@ const loginHandler = require("./routes/auth");
 // Collega il router all'app principale
 app.use("/", pages);
 
-app.post("/upload", multer({ dest: './uploads/' }).single("immagine"), (req, res) => {  // per gli eventi json
-    const { titolo, categoria, data_inizio, data_fine, luogo, descrizione } = req.body.evento;
-    db.query(
-        'INSERT INTO eventi (titolo, categoria, data_inizio, data_fine, luogo, descrizione) VALUES (?, ?, ?, ?, ?, ?)',
-        [titolo, categoria, data_inizio, data_fine, luogo, descrizione],
-        (err, result) => {
-          if (err) {
-            console.error('Errore durante l\'inserimento dell\'evento nel database MySQL:', err);
-            res.status(500).send('Errore durante l\'inserimento dell\'evento nel database MySQL');
-            return;
-          }
-          console.log('Nuovo evento inserito nel database MySQL');
-          res.status(201).send('Evento inserito correttamente nel database MySQL');
-        }
-      );
-})
+app.post(
+    "/upload",
+    multer({ dest: "./uploads/" }).single("immagine"),
+    (req, res) => {
+        // per gli eventi json
+        const evento = JSON.parse(req.body.evento);
+        const immagine = req.file.path; // Ottieni il percorso del file dell'immagine caricata
+        db.query(
+            "INSERT INTO eventi SET ?",
+            [{ ...evento, immagine: immagine }], // Aggiungi il percorso del file all'array di valori
+            (err, result) => {
+                if (err) {
+                    console.error(
+                        "Errore durante l'inserimento dell'evento nel database MySQL:",
+                        err
+                    );
+                    res.status(500).send(
+                        "Errore durante l'inserimento dell'evento nel database MySQL"
+                    );
+                    return;
+                }
+                console.log("Nuovo evento inserito nel database MySQL");
+                res.status(201).send(
+                    "Evento inserito correttamente nel database MySQL"
+                );
+                console.log("files", req.file);
+                console.log("evento", req.body.evento);
+                res.ren
+            }
+        );
+    }
+);
 
 // operazioni sul database backend
-app.use("/api", api); 
+app.use("/api", api);
 app.post("/auth", loginHandler);
 
 // Gestione degli errori per le route non trovate
